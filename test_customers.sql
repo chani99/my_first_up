106876101d8674882bc3ea2b464a108dd29b4e31
chani <click0548450396@gmail.com>
2017-07-02 18:54:50 +0300

    hi

--- test_customers.sql
+++ test_customers.sql
@@ -4,6 +4,9 @@
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.1.22-MariaDB
 
+____________
+Hi Chani!!!!!!!!!!!!!!!!!!!
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
